624bf8920fbc5d9a12d140afc142ef5821ce93d1
Deevanshu <deevanshu1997@gmail.com>
2021-08-26 14:41:53 +0530

    added all controllers files

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,20 +1,9 @@
 const express = require("express");
 
-// const Product = require("../models/product.model");
-
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  //   const products = await Product.find({}).lean();
-  //   res.json(products);
-  return res.render("ejs/home", {
-    // products: products,
-  });
+  return res.render("ejs/home", {});
 });
-
-// router.post("/", async (req, res) => {
-//   const product = await Product.create(req.body);
-//   res.json(product);
-// });
 
 module.exports = router;

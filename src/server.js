062e1b2cf68ef0a93488367b230b94749949aade
Deevanshu <deevanshu1997@gmail.com>
2021-08-26 14:59:21 +0530

    all layouts brought back to the node app, need to work on functionalities

--- src/server.js
+++ src/server.js
@@ -1,22 +1,43 @@
+// Import the important packages
 const path = require("path");
 const express = require("express");
 
+// Import the configration file
 const connect = require("./configs/db");
 
+// Import the controllers
 const productController = require("./controllers/product.controller");
 const homeController = require("./controllers/home.controller");
+const addressController = require("./controllers/address.controller");
+const bagController = require("./controllers/bag.controller");
+const loginController = require("./controllers/login.controller");
+const mensControllers = require("./controllers/mens.controller");
+const modaControllers = require("./controllers/moda.controller");
+const paymentController = require("./controllers/payment.controller");
+const signinController = require("./controllers/signin.controller");
 
+// Create the express app using middleware
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/static", express.static(path.join(__dirname, "public")));
 
+// Create the views part of the application
 app.set("views", path.join(__dirname, "views/"));
 app.set("view engine", "ejs");
 
+// Create the routes
 app.use("/products", productController);
 app.use("/home", homeController);
+app.use("/address", addressController);
+app.use("/bag", bagController);
+app.use("/login", loginController);
+app.use("/mens", mensControllers);
+app.use("/moda", modaControllers);
+app.use("/payment", paymentController);
+app.use("/signin", signinController);
 
+// Listen for events
 app.listen(3000, async () => {
   await connect();
   console.log("Server is running on port 3000");

0afb2bb63583f3e8a79dc79c90f7951dd6fa4cda
Deevanshu <deevanshu1997@gmail.com>
2021-08-28 12:10:54 +0530

    token decoded, localstorage will get userInfo

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -36,8 +36,10 @@ router.post("/login", async (req, res) => {
       }
     ).lean();
 
+    const userInfo = jwt.verify(token, JWT_SECRET);
+
     // localStorage.setItem("token", token);
-    return res.json({ status: "ok", data: token });
+    return res.json({ status: "ok", data: userInfo });
 
     // req.session.user = user;
     // res.redirect("/");

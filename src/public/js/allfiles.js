19fd71c722da5c5472da681b4db54034084b7c76
imbickydutta <imbickydutta@gmail.com>
2021-08-28 22:59:38 +0530

    new routes

--- src/public/js/allfiles.js
+++ src/public/js/allfiles.js
@@ -1,31 +1,33 @@
-function sort() {
+function sort(x) {
   let menu = document.getElementById("type");
   menu.addEventListener("change", generateData);
 
   function generateData(event) {
     if (menu.value == "1") {
-      window.location.href = `/products/sorthtol/tshirt/`;
+      window.location.href = `/products/sorthtol/${x}/`;
     } else if (menu.value == "2") {
-      window.location.href = `/products/sortltoh/tshirt/`;
+      window.location.href = `/products/sortltoh/${x}/`;
     } else if (menu.value == "3") {
-      window.location.href = `/products/sort/tshirt/`;
+      window.location.href = `/products/sort/${x}/`;
     } else if (menu.value == "4") {
-      window.location.href = `/products/type/tshirt/`;
+      window.location.href = `/products/type/${x}/`;
     }
   }
 }
 sort();
 
-function filter(x) {
-  window.location.href = `/products/type/tshirt/${x}`;
+
+// funtions for mens page
+function filter(x, y) {
+  window.location.href = `/products/type/${y}/${x}`;
 }
 
-function priceFilter(x, y) {
-  window.location.href = `/products/price/${x}/${y}/tshirt/`;
+function priceFilter(x, y, z) {
+  window.location.href = `/products/price/${x}/${y}/${z}/`;
 }
 
-function colorFilter(x) {
-  window.location.href = `/products/color/${x}/tshirt/`;
+function colorFilter(x, y) {
+  window.location.href = `/products/color/${x}/${y}/`;
 }
 
 function nextPage(id) {
@@ -35,3 +37,6 @@ function nextPage(id) {
 function changePage(x) {
   window.location.href = `/moda/${x}`;
 }
+
+// functions for womens sarees
+

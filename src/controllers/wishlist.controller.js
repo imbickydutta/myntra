0465d180941d6cc2aeb457955cddf320064b78a4
Deevanshu <deevanshu1997@gmail.com>
2021-08-29 14:34:04 +0530

    empty wishlist added

--- src/controllers/wishlist.controller.js
+++ src/controllers/wishlist.controller.js
@@ -8,50 +8,50 @@ const Product = require("../models/product.model");
 
 const router = express.Router();
 
-
+router.get("/", (req, res) => {
+  return res.render("ejs/emptyWL");
+});
 
 router.get("/:userId", async (req, res) => {
-    try {
-        let user = await User.findById(req.params.userId);
+  try {
+    let user = await User.findById(req.params.userId);
 
-        let bag = user.wishListItems
+    let bag = user.wishListItems;
 
-        // console.log(bag);
+    // console.log(bag);
 
-        let prodArr = []
-        for (let i = 0; i < bag.length; i++) {
-            let product = await Product.findById(bag[i].productId);
-            prodArr.push([product, 1])
-
-        }
-
-        return res.render("ejs/wishlist", { items: prodArr });
-    }
-    catch (err) {
-        res.send(err.message);
+    let prodArr = [];
+    for (let i = 0; i < bag.length; i++) {
+      let product = await Product.findById(bag[i].productId);
+      prodArr.push([product, 1]);
     }
 
+    return res.render("ejs/wishlist", { items: prodArr });
+  } catch (err) {
+    res.send(err.message);
+  }
 });
 
 router.post("/add", async (req, res) => {
+  let { userId, prodId } = req.body;
+  let user = await User.findById(userId).lean().exec();
 
-    let { userId, prodId } = req.body;
-    let user = await User.findById(userId).lean().exec();
+  let bag = user.wishListItems;
 
-
-    let bag = user.wishListItems
-
-    for (let i = 0; i < bag.length; i++) {
-
-        if (bag[i].productId == prodId) {
-
-            return res.send("Item Already Added")
-        }
+  for (let i = 0; i < bag.length; i++) {
+    if (bag[i].productId == prodId) {
+      return res.send("Item Already Added");
     }
+  }
 
-    user = await User.findByIdAndUpdate(userId, { wishListItems: [...bag, { productId: prodId }] }, { new: true }).lean().exec();
-    return res.json(user)
-
+  user = await User.findByIdAndUpdate(
+    userId,
+    { wishListItems: [...bag, { productId: prodId }] },
+    { new: true }
+  )
+    .lean()
+    .exec();
+  return res.json(user);
 });
 
 // router.post("/bag", async (req, res) => {
